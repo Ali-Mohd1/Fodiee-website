cfaf4c42a6b7f9dc91ea5448070e6f97e0993653
Ali-Mohd1 <ali.beigh123@gmail.com>
2021-01-23 14:56:12 +0530

    updated html

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// Scrollspy
+var spy = new Gumshoe('nav a');
+// Burger
+    $('.burger').on('click', function (event) {
+        $('.navigation-bar').slideToggle('200');
+    })
+
+// Sticky navbar
+$(document).ready(function(){
+	$(window).bind('scroll', function() {
+		var navHeight = $('header').height();
+		if ($(window).scrollTop() > navHeight) {
+			$('header').addClass('fixed');
+		 }
+		else {
+			$('header').removeClass('fixed');
+		 }
+	});
+});
+// Scroll to TOP
+var btn = $('.scrollup');
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '500');
+});
+    // Reveal animation
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '40px',
+        duration: 1500,
+        reset: true
+    });
+    ScrollReveal().reveal('.col-left, .content-col, .img-col', { origin: 'left' });
+    ScrollReveal().reveal('.col-right, .content-col-image, .getapp-content', { origin: 'right' });
+    ScrollReveal().reveal('.item, .footer-about, .footer-col', { interval: 100 });
+   
+
+
+
